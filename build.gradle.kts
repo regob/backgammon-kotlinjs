82e95889e24b7c93660e6aecd5e3b8fde63d7bd0
regob <rego97@gmail.com>
2022-10-20 19:59:10 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("js") version "1.7.10"
+}
+
+group = "me.regob"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:kotlinx-html:0.7.2")
+}
+
+kotlin {
+    js(LEGACY) {
+        binaries.executable()
+        browser {
+            commonWebpackConfig {
+                cssSupport.enabled = true
+            }
+        }
+    }
+}
