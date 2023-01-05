9247adb8b02f7291ce5a7f7aedb1514c930c115b
regob <rego97@gmail.com>
2023-01-05 14:47:31 +0100

    add favicon and modify timing params in frontend

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -1,11 +1,11 @@
 import kotlinx.browser.window
 
 // animation parameters for each element (in ms)
-val ANIM_DURATION_DICE = 1000
-val ANIM_FREQ_DICE = 150
+val ANIM_DURATION_DICE = 800
+val ANIM_WAIT_AFTER_DICE = 800
+val ANIM_FREQ_DICE = 75
 val ANIM_DURATION_MOVE = 1000
 val ANIM_FREQ_MOVE = 22
-val ANIM_DURATION_NEW_ROUND = 1000
 val ANIM_DURATION_NEW_TURN = 400
 val ANIM_DURATION_ROUND_END = 1500
 val ANIM_DURATION_GAME_END = 1500
@@ -13,7 +13,7 @@ val ANIM_DURATION_GAME_END = 1500
 
 
 fun main() {
-    val app: App = App()
+    val app = App()
     window.onload = {
         app.start()
     }
